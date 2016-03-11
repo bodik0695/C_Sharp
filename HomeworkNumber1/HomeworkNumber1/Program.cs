b4edcb9162bdeb1d1357ed17f37d142ff71671ad
bodik0695 <bodik9595@gmail.com>
2016-03-11 07:45:34 +0200

    Homework 1

---
+++ HomeworkNumber1/HomeworkNumber1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkNumber1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lengthArr;
+            Console.Write("Введите длину массива: ");
+            string readLengthArr = Console.ReadLine();
+            if (Int32.TryParse(readLengthArr, out lengthArr))
+            {
+                int[] array = new int[lengthArr];
+                Random rand = new Random();
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = rand.Next(1, 100);
+                    Console.WriteLine(array[i]);
+                }
+                Sort.QuickSort(array, 0, array.Length - 1);
+                Console.WriteLine("Отсортированный масив: ");
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не цифру");
+            }
+        }
+    }
+}
---
+++ HomeworkNumber1/HomeworkNumber1/Sort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkNumber1
+{
+   static class Sort
+    {
+        public static int Fragmentation(int[] array, int start, int end)
+        {
+            int pointer = start;
+            for (int i = start; i <= end; i++)
+            {
+                if (array[i] <= array[end])
+                {
+                    int temp = array[pointer];
+                    array[pointer] = array[i];
+                    array[i] = temp;
+                    pointer += 1;
+                }
+            }
+            return pointer - 1;
+
+        }
+        public static void QuickSort(int[] array, int start, int end)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+            int RefElement = Fragmentation(array, start, end);
+            QuickSort(array, start, RefElement - 1);
+            QuickSort(array, RefElement + 1, end);
+        }
+    }
+}
